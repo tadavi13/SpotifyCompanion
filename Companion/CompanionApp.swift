9a6a7cff14eab9e64bacfbf1f9de849f910303f7
Tyler Davis <tylerd21304@gmail.com>
2024-07-26 15:24:35 -0400

    Initial Commit

---
+++ Companion/CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionApp.swift
+//  Companion
+//
+//  Created by Admin on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
